4433387f0bfd082181cd796884ecbe48f1a41b14
mattlaguardia <matthewlaguardia@gmail.com>
2016-09-06 23:35:57 -0700

    fixes small bugs

--- popup.js
+++ popup.js
@@ -3,23 +3,18 @@ var localStorageName = "urls";
 function loadUrls(){
   // fetch urls from text area and load them //
   var urls = localStorage[localStorageName];
-
   if (urls == undefined){
     urls = "";
   } else {
-    urls = JSON.parse(urls);
+    urls = urls.split(",");
   }
-
   document.getElementById("urls").value = urls;
-
-}
-function saveUrls(){
-  // fetch urls and save it in chrome storage //
-  var str = document.getElementById("urls").value;
-  var urls = str.slit(/\r\n?/g);
-
-  localStorage[localStorageName] = JSON.stringify(urls);
 }
 
+function addUrls () {
+  var selector = document.getElementById("urls").value;
+  localStorage.setItem("urls", selector);
+}
+
+document.getElementById("button").addEventListener("click", addUrls);
 document.getElementById("button").addEventListener("click", loadUrls);
-document.getElementById("button").addEventListener("click", saveUrls);
